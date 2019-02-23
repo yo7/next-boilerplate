13547061c69e27a229cdf17188b8ce9e3da20090
yo7 <bloom6445@gmail.com>
2019-02-23 16:25:30 +0900

    add styled-components and css prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+/// <reference types="styled-components/cssprop" />
+
 import * as React from 'react'
+import {css} from 'styled-components'
 
 export default () => {
   return (
-    <div>Hello world!!</div>
+    <div css={css`
+      color: red;
+    `}>Hello world!!</div>
   )
 }
